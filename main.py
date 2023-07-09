4fe1bb92c14f33d3d0d826db09520d8578bea8d7
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-07-09 23:13:29 +0800

    Use github action

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
 import json
 import os
-
-from typing import Dict, List, Tuple, Any, Awaitable
+import random
+from typing import Dict, List, Tuple, Any
 import httpx
+from anyio import EndOfStream
 
 SCAN_CHUNK_SIZE = 64
-GET_INFO_NUM = 4
-
+GET_INFO_NUM = 16
+COOKIES = {}
 IdLeft = int  # Id大于等于的序号
 IdRight = int | None  # Id小于的序号
 Info = Dict[str, Any]
@@ -30,151 +31,199 @@ class Unknown(Exception):
     pass
 
 
-async def get_info(id: int, ignore_not_found=False, ignore_created=False) -> Info:
-    r = None
-    try:
-        async with httpx.AsyncClient() as client:
-            r = await client.get(
-                f"https://www.ctfer.vip/api/problem/{id}/",
-                headers={
-                    "UserAgent": "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36",
-                    "Cookie": os.environ["NSSCTF_COOKIE"]
-                }
-            )
-    except Exception:
-        await asyncio.sleep(2)
-        return await get_info(id, ignore_not_found, ignore_created)
-    try:
-        data = r.json()
-    except Exception:
-        raise Unknown("parse json failed")
+USER_AGENT = (
+    "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 "
+    + "(KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36"
+)
 
+
+async def login():
+    """使用账号密码登陆
+
+    Returns:
+        _type_: _description_
+    """
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            "https://www.nssctf.cn/api/user/login/",
+            headers={"User-Agent": USER_AGENT},
+            data={
+                "username": os.environ["NSSCTF_USERNAME"],
+                "password": os.environ["NSSCTF_PASSWORD"],
+            },
+        )
+        cookies = dict(resp.cookies)
+        cookies["token"] = resp.json()["data"]["token"]
+        return cookies
+
+
+async def get_info(
+    info_id: int, ignore_not_found=False, ignore_created=False
+) -> Info | None:
+    """根据id获取对应题目的信息
+
+    Args:
+        info_id (int): id
+        ignore_not_found (bool, optional): 找不到时返回空. Defaults to False.
+        ignore_created (bool, optional):
+            忽略CreatedButNotFound的情况. Defaults to False.
+
+    Raises:
+        NotLogin: 没有登陆
+        NotFound: 没有找到
+        CreatedButEmpty: 找到了，但是为空
+        Unknown: 未知
+
+    Returns:
+        Info | None: 返回的信息
+    """
+    resp = None
+    try:
+        async with httpx.AsyncClient(cookies=COOKIES) as client:
+            resp = await client.get(
+                f"https://www.nssctf.cn/api/problem/v2/{info_id}/",
+                headers={
+                    "UserAgent": USER_AGENT,
+                    # "Cookie": os.environ["NSSCTF_COOKIE"],
+                },
+            )
+    except (httpx.HTTPError, EndOfStream) as exc:
+        print(f"error: {type(exc)} {exc}")
+        await asyncio.sleep(2)
+        return await get_info(info_id, ignore_not_found, ignore_created)
+    try:
+        data = resp.json()
+    except Exception as exc:
+        raise Unknown("parse json failed") from exc
     if data.get("code", None) == 402:
         raise NotLogin()
     if "data" not in data:
         if ignore_not_found:
-            return {}
+            return None
         raise NotFound()
     if "code" not in data:
-        raise Unknown(f"code not found for id: {id}")
+        raise Unknown(f"code not found for id: {info_id}")
     if data["code"] == 201:
         if ignore_created:
-            return {}
+            return None
         raise CreatedButEmpty()
     if data["code"] != 200:
-        raise Unknown(f"code is not 200 for id: {id}, data:{data}")
+        raise Unknown(f"code is not 200 for id: {info_id}, data:{data}")
     try:
-        d = data["data"]
+        info_data = data["data"]
         info = {
-            "id": id,
-            "title": d["title"],
-            "desc": d["desc"],
-            "tags": d["tag"],
-            "point": d["point"],
-            "likes": d["likes"],
-            "level": d["level"],
-            "solved": d["info"]["solved"],
-            "wa": d["info"]["wa"],
+            "id": info_id,
+            "title": info_data["title"],
+            "desc": info_data["desc"],
+            "tags": [tag_info[0] for tag_info in info_data["tag"]],
+            "point": info_data["point"],
+            "likes": info_data["likes"],
+            "level": info_data["level"],
+            "solved": info_data["info"]["solved"],
+            "wa": info_data["info"]["wa"],
         }
-    except Exception:
-        raise Unknown("parse info failed")
+    except Exception as exc:
+        raise Unknown("parse info failed") from exc
     return info
 
 
-async def scan(context: ScanContext) -> Tuple[List[asyncio.Task[Info]], ScanContext | None]:
-    l, r = context
-    if r == None:
-        try:
-            _ = await get_info(l + SCAN_CHUNK_SIZE)
-        except NotFound:
-            new_context = (l, l + SCAN_CHUNK_SIZE)
-            return await scan(new_context)
-        except CreatedButEmpty:
-            pass
-        new_context = (l + SCAN_CHUNK_SIZE, None)
-        return (
-            [
-                asyncio.Task(get_info(
-                    i,
-                    ignore_not_found=True,
-                    ignore_created=True
-                )) for i in range(l, l + SCAN_CHUNK_SIZE)
-            ],
-            new_context
-        )
-    if r - l == 1:
-        return (
-            [
-                asyncio.Task(get_info(
-                    i,
-                    ignore_not_found=True,
-                    ignore_created=True
-                )) for i in range(l, r)
-            ],
-            None
-        )
-    mid = (l + r) // 2
-    try:
-        info = await get_info(mid)
-    except NotFound:
-        new_context = (l, mid)
-        return await scan(new_context)
-    return (
-        [
-            asyncio.Task(get_info(i, ignore_not_found=True)) for i in range(l, mid)
-        ],
-        (mid, r)
-    )
-
-
-# async def scan_all():
-#     context = (1, None)
-#     undone_tasks = []
-#     while context:
-#         tasks, context = await scan(context)
-#         undone_tasks += tasks
-#         done_tasks, undone_tasks = [task for task in tasks if task.done()], [task for task in tasks if not task.done()]
-#         while not done_tasks:
-#             done_tasks, undone_tasks = [task for task in tasks if task.done()], [task for task in tasks if not task.done()]
-#         for task in done_tasks:
-#             yield await task
-#         print(f"{context=}")
-
-
-async def scan_all(max_id=10000, early_stop=500):
+async def task_slide_window(coro_gen, window_size):
+    """从coro_gen中迭代协程，创建并执行对应的task, 并yield结果"""
     tasks: List[asyncio.Task] = []
-    last_exist_info_id = 0
-    for i in range(1, max_id):
-        task = asyncio.create_task(
-            get_info(i, ignore_created=True, ignore_not_found=True))
+    for coro in coro_gen:
+        task = asyncio.create_task(coro)
         tasks.append(task)
-        while not [task for task in tasks if task.done()] and len(tasks) > GET_INFO_NUM:
+        while (
+            not [task for task in tasks if task.done()]
+            and len(tasks) > window_size
+        ):
             await asyncio.sleep(0)
         done_tasks, tasks = [task for task in tasks if task.done()], [
-            task for task in tasks if not task.done()]
+            task for task in tasks if not task.done()
+        ]
         for task in done_tasks:
-            info = await task
-            if info and info.get("id", None):
-                last_exist_info_id = max(last_exist_info_id, info["id"])
+            yield await task
+
+
+async def scan_all(from_id=1, max_id=10000, early_stop=500):
+    """扫描整个网站
+
+    Args:
+        max_id (int, optional): 扫描使用的最大id. Defaults to 10000.
+        early_stop (int, optional): 有多少个无效ID则提早停止. Defaults to 500.
+
+    Yields:
+        Info: 扫描到的题目信息
+    """
+    nonexist_count = 0
+
+    def coro_gen():
+        for i in range(from_id, max_id):
+            coro = get_info(i, ignore_created=True, ignore_not_found=True)
+            should_stop = yield coro
+            if should_stop:
+                break
+
+    gen = coro_gen()
+    async for info in task_slide_window(gen, window_size=GET_INFO_NUM):
+        if info and info.get("id", None):
             yield info
-        if early_stop and i - last_exist_info_id > early_stop:
-            return
+        else:
+            nonexist_count += 1
+        if nonexist_count > early_stop:
+            try:
+                _ = gen.send(True)
+            except StopIteration:
+                pass
 
 
 async def main():
+    """异步爬虫的入口"""
+    global COOKIES
     infos = []
-    async for info in scan_all():
+    with open("infos.json", "r", encoding="utf-8") as file:
+        infos: List[Info] = json.load(file)
+    ids = set(info["id"] for info in infos)
+    current_max_id = max(ids)
+    print(f"max id of now: {current_max_id}")
+
+    # 登陆
+    COOKIES = await login()
+
+    # 刷新已经有了的ID
+    target_id_digit = random.randint(0, 9)
+    coros = (get_info(i) for i in ids if i % 10 == target_id_digit)
+    refreshed_infos: List[Info] = []
+    refreshed_ids = set()
+
+    async for info in task_slide_window(coros, window_size=GET_INFO_NUM):
+        if info is None:
+            continue
+        print(info.get("id"), info.get("title"))
+        refreshed_infos.append(info)
+        refreshed_ids.add(info["id"])
+
+    infos = [
+        info for info in infos if info["id"] not in refreshed_ids
+    ] + refreshed_infos
+    infos.sort(key=lambda info: info["id"])
+
+    # 获取当前没有的题目
+    async for info in scan_all(from_id=current_max_id):
         if not info:
             continue
         print(info.get("id"), info.get("title"))
         infos.append(info)
-    with open("infos.json", "w") as f:
-        json.dump(infos, f, indent=2, ensure_ascii=False)
+
+    with open("infos.json", "w", encoding="utf-8") as file:
+        json.dump(infos, file, indent=2, ensure_ascii=False)
 
 
 async def test():
-    async for info in scan_all():
-        print(info)
+    """测试函数入口"""
+    info = await get_info(382)
+    print(info)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
